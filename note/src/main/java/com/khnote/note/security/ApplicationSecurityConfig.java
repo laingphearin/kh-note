462087d7df8fcf36b0ed78fe1562bd6bba04e235
Laing Phearin <laingphearin@gmail.com>
2022-07-28 08:19:45 +0700

    F Implement Logout

--- note/src/main/java/com/khnote/note/security/ApplicationSecurityConfig.java
+++ note/src/main/java/com/khnote/note/security/ApplicationSecurityConfig.java
@@ -3,15 +3,22 @@ package com.khnote.note.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.antMatcher("/**")
                 .authorizeRequests(a -> a
                         .antMatchers("/swagger-ui/index.html", "/swagger-ui/*", "/v3/api-docs", "/v3/api-docs/*", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/")
                 )
                 .oauth2Login();
     }
