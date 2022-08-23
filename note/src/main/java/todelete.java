b57028a8d910aad6a54e6b287fd07b5f5978e825
Laing Phearin <laingphearin@gmail.com>
2022-08-23 13:37:27 +0700

    feature-a

---
+++ note/src/main/java/todelete.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class todelete {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1,1,2);
+        Set<Integer> distinct = new HashSet<>(numbers);
+    }
+}
